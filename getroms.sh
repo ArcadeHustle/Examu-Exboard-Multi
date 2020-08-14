f73865c4457ad46c96a38014ce3560c81afbf613
Hostile Individual <hostile@me.com>
2020-08-14 15:53:23 -0400

    Add get rom script to pull from archive org

---
+++ getroms.sh
@@ -0,0 +1,3 @@
+wget https://archive.org/download/Examu-Exboard-XPe-ROM-archive/ArcanaHeartVer2.1.exboard.img
+wget https://archive.org/download/Examu-Exboard-XPe-ROM-archive/DemonBride.exboard.img
+
