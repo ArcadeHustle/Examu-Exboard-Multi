b7e807579effcba917ca186fcd875a9ea5d7c090
hostile <Mavproxyuser>
2020-09-03 22:49:20 -0400

    Add example rando launch

---
+++ main.c
@@ -0,0 +1,71 @@
+// apt-get install binutils-mingw-w64-i686 g++-mingw-w64-i686 gcc-mingw-w64-i686
+// i686-w64-mingw32-gcc main.c -o main.exe --static
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+    printf("FuckA DarkSoft\n");
+    FILE *fin;
+    FILE *fout;
+    int cur;
+    int next;
+    if( access( "fuckadarksoft", F_OK ) != -1 )
+    {
+    	fin = fopen("fuckadarksoft", "r");
+    	cur = fgetc(fin);
+    	fclose(fin);
+    }
+    else
+    {
+        fout = fopen("fuckadarksoft", "w");
+        cur = 0;
+        next=1;
+        fputc(next,fout);
+        fclose(fout);
+    }
+    if(cur==0)
+    {
+        printf("Entry 0 - DB1");
+        fout = fopen("fuckadarksoft", "w");
+        next=1;
+        fputc(next,fout);
+        fclose(fout);
+        execl("C:/DB1.exe","DB1",0);
+    }
+    else if(cur==1)
+    {
+        printf("Entry 1 - AH2");
+        fout = fopen("fuckadarksoft", "w");
+        next=2;
+        fputc(next,fout);
+        fclose(fout);
+        execl("D:/AH2.exe","AH2",0);
+    }
+    else if(cur==2)
+    {
+        printf("Entry 2 - AH3");
+        fout = fopen("fuckadarksoft", "w");
+        next=3;
+        fputc(next,fout);
+        fclose(fout);
+        execl("E:/AH3.exe","AH3",0);
+    }
+    else if(cur>2)
+    {
+        printf("Catch All (Entry 0) - DB1");
+        fout = fopen("fuckadarksoft", "w");
+        next=1;
+        fputc(next,fout);
+        fclose(fout);
+        execl("C:/DB1.exe","DB1",0);
+    }
+    else
+    {
+        printf("Makes no sense cur is %x\n, Launch DB1",cur);
+        execl("C:/DB1.exe","DB1",0);
+    }
+
+}
+
