f8ab2d2042163c5b5085bd3b070863b0a9300da9
hostile <Mavproxyuser>
2020-09-13 01:14:43 -0400

    filter JAMMA input

--- JAMMA_dump.py
+++ JAMMA_dump.py
@@ -10,6 +10,8 @@
 #	replyBuffer.push(control_byte_3());
 #	replyBuffer.push(0x42);
 
+from __future__ import print_function
+
 import serial
 import binascii
 
@@ -19,12 +21,31 @@ ser = serial.Serial(
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
-    timeout = None
+    timeout = 0.2
 )
 
 while True:
     ser_bytes = ser.read_until('\x42')
-    print(binascii.hexlify(ser_bytes).split('\x5a'))
 
+    if len(ser_bytes) < 2:
+        continue
+
+#    print(binascii.hexlify(ser_bytes[0:7]))
+
+    if ser_bytes[-2] == '\x00':
+        # 76fd080000000042 - Idle stick input 
+        if ser_bytes[0:7] == '\x76\xfd\x08\x00\x00\x00\x00':
+              continue
+    	      print(binascii.hexlify(ser_bytes))
+        else:
+    	      print(binascii.hexlify(ser_bytes))
+    elif ser_bytes[-2] == '\x20':
+    	print(binascii.hexlify(ser_bytes))
+    elif ser_bytes[-2] == '\x70':
+    	print(binascii.hexlify(ser_bytes))
+    else:
+    	print(binascii.hexlify(ser_bytes))
+    
+ 
 
 
